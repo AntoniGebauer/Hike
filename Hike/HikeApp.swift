59d2ec25baf25337a8bfabe14b8712cb133f3d8f
Antoni Gebauer <antoni.gebauer@comarch.com>
2023-10-31 16:36:35 +0100

    Initial Commit

---
+++ Hike/HikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikeApp.swift
+//  Hike
+//
+//  Created by Antoni Gebauer on 31/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct HikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
